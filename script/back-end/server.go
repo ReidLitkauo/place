6daa21bbd0adc1f9c700b1acd5b35a7ff2cbb4f1
ReidLitkauo <reid.litkauo@gmail.com>
2022-05-28 08:16:56 -0700

    Final commit

--- script/back-end/server.go
+++ script/back-end/server.go
@@ -8,6 +8,8 @@ import (
 func main() {
     http.Handle("/", http.FileServer(http.Dir("./public")))
 	fmt.Println("Listening...")
+	// TODO If any improvements need to be made to this stub server,
+	// move the port to its own config file.
     http.ListenAndServe(":8090", nil)
 }
 
